81163b6a374f379bafc09e632a13dde2138225e6
Nemya <yiduolnemya@gmail.com>
2025-01-11 21:06:19 -0500

    feat: initialize openai api

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// server.js
+
 import express from "express";
 import cors from "cors";
+import { OpenAI } from "openai";
 
 const app = express();
 const port = process.env.PORT || 3005;
+const apiKey = process.env.VITE_OPEN_AI_KEY;
+const openai = new OpenAI({ apiKey: apiKey });
 
 app.use(cors());
 app.use(express.json());
