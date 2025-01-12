c7e26e6d7226cb63e05d1e94ff3e52d16ac7f6c6
Nemya <yiduolnemya@gmail.com>
2025-01-11 21:08:41 -0500

    feat: add post request for user messages
    Code is copied directly from the OPENAI documentation

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// server.js
-
 import express from "express";
 import cors from "cors";
 import { OpenAI } from "openai";
@@ -13,4 +11,23 @@ app.use(cors());
 app.use(express.json());
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+app.post("/chatbot", async (req, res) => {
+  const { question } = req.body;
+  const response = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: question,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+    max_tokens: 300,
+  });
+  res.send(response.choices[0].message.content);
 });
