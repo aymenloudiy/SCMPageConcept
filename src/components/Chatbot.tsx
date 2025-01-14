b687a0aadecbea1192972ec790300ab9b5a5e30d
Nemya <yiduolnemya@gmail.com>
2025-01-13 22:28:42 -0500

    feat: add user input and it's state

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 export default function Chatbot() {
+  const [response, setResponse] = useState<string>("Hi! How can I assist you?");
+  const [value, setValue] = useState<string>("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value);
   return (
     <div>
       <div className="container">
         <div>
-          <input type="text"></input>
+          <input type="text" value={value} onChange={onChange}></input>
         </div>
         <div>
           <button>Send</button>
         </div>
         <div>
-          <p>Chatbot:</p>
+          <p>Chatbot:{response}</p>
         </div>
       </div>
     </div>
