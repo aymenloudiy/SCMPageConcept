11e7298a225ae9b28850a561a2bc3ba389e7421e
Nemya <yiduolnemya@gmail.com>
2025-01-13 22:25:54 -0500

    feat: add chatbot html skeleton

---
+++ src/components/Chatbot.tsx
@@ -0,0 +1,17 @@
+export default function Chatbot() {
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <input type="text"></input>
+        </div>
+        <div>
+          <button>Send</button>
+        </div>
+        <div>
+          <p>Chatbot:</p>
+        </div>
+      </div>
+    </div>
+  );
+}
