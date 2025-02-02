e9d84eced6303a40f224ec09c3403af376885d1b
Nemya <yiduolnemya@gmail.com>
2025-02-01 19:12:19 -0500

    feat: start psotgre database
    chore: add dotenv dependency and use it

---
+++ server/models/database.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "127.0.0.1",
+    dialect: "postgres",
+    logging: true,
+  }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected successfully.");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+};
+
+export default { sequelize, connectDB };
