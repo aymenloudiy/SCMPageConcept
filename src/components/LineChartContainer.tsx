fb60b589655ba876480182d7181fcc3bc30e8568
Nemya <yiduolnemya@gmail.com>
2025-02-02 18:02:54 -0500

    style: fix color contrast

--- src/components/LineChartContainer.tsx
+++ src/components/LineChartContainer.tsx
@@ -1,15 +1,36 @@
 import { LineChart } from "@mui/x-charts/LineChart";
+import { grey } from "@mui/material/colors";
+
 export default function LineChartContainer() {
   return (
     <div className="col-span-9">
       <LineChart
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        xAxis={[
+          {
+            data: [1, 2, 3, 5, 8, 10],
+            sx: {
+              ".MuiChartsAxis-tickLabel": { fill: grey[100] },
+              ".MuiChartsAxis-line": { stroke: grey[100] },
+            },
+          },
+        ]}
+        yAxis={[
+          {
+            sx: {
+              ".MuiChartsAxis-tickLabel": { fill: grey[100] },
+              ".MuiChartsAxis-line": { stroke: grey[100] },
+            },
+          },
+        ]}
         series={[
           {
             data: [2, 5.5, 2, 8.5, 1.5, 5],
             area: true,
           },
         ]}
+        sx={{
+          "& .MuiChartsLegend-root": { color: grey[100] },
+        }}
       />
     </div>
   );
