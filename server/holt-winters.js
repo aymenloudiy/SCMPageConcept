d689a13d888b1c9e22105e2271302a92237d4e09
Nemya <yiduolnemya@gmail.com>
2025-02-02 11:11:21 -0500

    chore: refer to changes
    The changes are reflected in the forked nostradmus repo.
    Link: https://github.com/aymenloudiy/Nostradamus.js

--- server/holt-winters.js
+++ server/holt-winters.js
@@ -1,5 +1,5 @@
 import forecast from "nostradamus";
-
+// the changes are reflected in the forked nostradmus repo https://github.com/aymenloudiy/Nostradamus.js
 var data = [
   362, 385, 432, 341, 382, 409, 498, 387, 473, 513, 582, 474, 544, 582, 681,
   557, 628, 707, 773, 592, 627, 725, 854, 661,
